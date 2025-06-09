59cf47c8cdb4ac99097b7a4639462ff76fc2ab88
sangarix <denya.garkusha.12@gmail.com>
2025-06-10 00:05:51 +0300

    fix: add cards

--- effects/21/main.js
+++ effects/21/main.js
@@ -21,7 +21,7 @@ const callback = (evt) => {
 card.addEventListener("mouseleave", () => {
   card.style.transform = `rotateX(0deg) rotateY(0deg) scale(1)`;
   img.style.transform =
-    "translate(0px 0px) rotateX(0deg) rotateY(0deg) scale(1)";
+    "translate(0px, 0px) rotateX(0deg) rotateY(0deg) scale(1)";
 });
 
 card.addEventListener("mousemove", callback);
