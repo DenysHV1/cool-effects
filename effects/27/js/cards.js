9125ecc17028e97165f9403d804d57bac490a3e4
sangarix <denya.garkusha.12@gmail.com>
2025-06-15 21:55:21 +0300

    add slider

---
+++ effects/27/js/cards.js
@@ -0,0 +1,35 @@
+const cards = () => {
+  const cards = document.querySelectorAll(".card");
+  const imgs = document.querySelectorAll(".card_box-image_item");
+  const MAX_ANGLE = 6;
+  cards.forEach((item, idx) => {
+    //! mousemove
+    item.addEventListener("mousemove", (evt) => {
+      const rect = evt.currentTarget.getBoundingClientRect();
+
+      const x = evt.clientX - rect.left;
+      const y = evt.clientY - rect.top;
+
+      const centerX = rect.width / 2;
+      const centerY = rect.height / 2;
+      const offsetX = -(x - centerX) / centerX;
+      const offsetY = (y - centerY) / centerY;
+      const rotateY = offsetX * MAX_ANGLE;
+      const rotateX = offsetY * MAX_ANGLE;
+
+      evt.currentTarget.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
+
+      imgs[idx].style.transform = `translate(${x * 0.1}px, ${
+        y * 0.1
+      }px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.3)`;
+    });
+
+    //! mouseleave
+    item.addEventListener("mouseleave", () => {
+      item.style.transform = `rotateX(0deg) rotateY(0deg) scale(1)`;
+      imgs[idx].style.transform =
+        "translate(0px, 0px) rotateX(0deg) rotateY(0deg) scale(1)";
+    });
+  });
+};
+export default cards;
---
+++ effects/27/js/const.js
@@ -0,0 +1,67 @@
+const array = [
+  {
+    img: "https://amanogawa.space/media/images/posters/2024/posters_thumbs/102.jpg",
+    name: "Монолог травниці1",
+  },
+  {
+    img: "https://amanogawa.space/media/images/posters/2024/posters_thumbs/poster_650oPIC.jpg",
+    name: "Дандадан2",
+  },
+  {
+    img: "https://amanogawa.space/media/images/posters/2025/posters_thumbs/poster.jpg",
+    name: "Тільки я візьму новий рівень 23",
+  },
+  {
+    img: "https://amanogawa.space/media/images/posters/2024/posters_thumbs/poster_qpuE76N.jpg",
+    name: "Машл: Магія і м'язи4",
+  },
+  {
+    img: "https://amanogawa.space/media/images/posters/2024/posters_thumbs/poster_zVpL3hq.jpg",
+    name: "Кайджу №85",
+  },
+  {
+    img: "https://amanogawa.space/media/images/posters/2023/posters_thumbs/poster_K3jekIa.jpg",
+    name: "Магічна битва6",
+  },
+  {
+    img: "https://amanogawa.space/media/images/posters/2024/posters_thumbs/poster_EPnZXkt.jpg",
+    name: "Тільки я візьму новий рівень7",
+  },
+  {
+    img: "https://amanogawa.space/media/images/posters/2024/posters_thumbs/poster_0Lost7Y.jpg",
+    name: "Вітролом8",
+  },
+  {
+    img: "https://amanogawa.space/media/images/posters/2024/posters_thumbs/poster_aJ5V3kG.jpg",
+    name: "Вежа бога9",
+  },
+  {
+    img: "https://amanogawa.space/media/images/posters/2025/posters_thumbs/poster_2xfshbQ.jpg",
+    name: "Наново10",
+  },
+  {
+    img: "https://amanogawa.space/media/images/posters/2025/posters_thumbs/poster_kISf0cY.jpg",
+    name: "Лазар11",
+  },
+  {
+    img: "https://amanogawa.space/media/images/posters/2025/posters_thumbs/poster_QKTABC8.jpg",
+    name: "Початок після кінця12",
+  },
+  {
+    img: "https://amanogawa.space/media/images/posters/2024/posters_thumbs/106.jpg",
+    name: "Сім'я шпигуна13",
+  },
+  {
+    img: "https://amanogawa.space/media/images/posters/2024/posters_thumbs/poster.jpg",
+    name: "Клас еліти14",
+  },
+  {
+    img: "https://amanogawa.space/media/images/posters/2025/posters_thumbs/poster_4Oar2Ig.jpg",
+    name: "Клинок зла15",
+  },
+  {
+    img: "https://amanogawa.space/media/images/posters/2025/posters_thumbs/poster.jpg",
+    name: "Ванпачмені16",
+  },
+];
+export default array;
