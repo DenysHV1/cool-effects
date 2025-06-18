8dedf5aa4728444c4532746e21b7cb8bfa896a85
sangarix <denya.garkusha.12@gmail.com>
2025-06-18 09:49:03 +0300

    add slider

--- effects/27/js/cards.js
+++ effects/27/js/cards.js
@@ -15,7 +15,7 @@ const cards = () => {
       const offsetX = -(x - centerX) / centerX;
       const offsetY = (y - centerY) / centerY;
       const rotateY = offsetX * MAX_ANGLE;
-      const rotateX = offsetY * MAX_ANGLE;
+      const rotateX = offsetY * MAX_ANGLE; 
 
       evt.currentTarget.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.03)`;
 
