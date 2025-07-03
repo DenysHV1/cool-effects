d67d5d35f99fda7d1d2b4631d7c11a8982fa9320
sangarix <denya.garkusha.12@gmail.com>
2025-07-03 19:17:57 +0300

    add fix opasity

--- effects/22/22.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelector(".input-js");
-
-document.body.style.backgroundColor = input.value;
-
-input.addEventListener("input", (e) => {
-  document.body.style.backgroundColor = e.target.value;
-});
